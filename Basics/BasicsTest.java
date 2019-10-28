e879d7ec1218f666de619dd531040d9c810a69f4
ibrahimha7 <43251034+ibrahimha7@users.noreply.github.com>
2019-10-28 23:19:26 +0300

    added basics

---
+++ Basics/BasicsTest.java
@@ -0,0 +1,25 @@
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.util.ArrayList;
+
+public class BasicsTest{
+
+    public static void main(String[] args){
+        Basics basics = new Basics();
+
+        basics.print1To255();
+        basics.printOdd1To255();
+        basics.printSum1To255();
+
+        int[] arr = {1, 2, 3, 4, -11, -9, 7};
+
+        basics.iterateArray(arr);
+        basics.findMax(arr);
+        basics.averageVal(arr);
+        basics.oddArray();
+        basics.squareVal(arr);
+        basics.greaterThanY(arr,2);
+        basics.noNegs(arr);
+        basics.maxMinAvg(arr);
+        basics.shiftOne(arr);
+    }
+}
