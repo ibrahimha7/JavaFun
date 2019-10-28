22c2bbe3caa14c1c645e6f60c5a1458ba7195131
ibrahimha7 <43251034+ibrahimha7@users.noreply.github.com>
2019-10-28 18:44:41 +0300

    Your own program

---
+++ Your Own Java Program/YourOwnJavaProgram.java
@@ -0,0 +1,12 @@
+public class YourOwnJavaProgram {
+    public static void main(String[] args) {
+        String name="Ibrahim";
+        Number age=23;
+        String hometwon="Faifa";
+
+        System.out.println("My Name is "+name);
+        System.out.println("I am "+age+" years old");
+        System.out.println("My hometwon is"+hometwon);
+
+    }
+}
