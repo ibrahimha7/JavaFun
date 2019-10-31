0fe59747bc36104a59a9d21c03040cddd2bb1116
ibrahimha7 <43251034+ibrahimha7@users.noreply.github.com>
2019-10-31 20:16:39 +0300

    added single linked list

---
+++ JavaOOP/SingleLinkedList/ListTest.java
@@ -0,0 +1,7 @@
+public class ListTest{
+    public static void main(String[] args){
+        SingleLinkedList list = new SingleLinkedList();
+        list.removeAt(0);
+        list.printValues();
+    }
+}
