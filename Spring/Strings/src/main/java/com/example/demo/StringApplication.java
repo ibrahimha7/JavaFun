bbd21a9997fe1c784b5c2b2a5223a3d256e05bc9
ibrahimha7 <43251034+ibrahimha7@users.noreply.github.com>
2019-11-04 20:57:27 +0300

    added strings

---
+++ Spring/Strings/src/main/java/com/example/demo/StringApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String visit() {
+		return "Hello Client ! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String rand() {
+		return "Spring Boot is greate !So easy to just respond with strings";
+	}
+
+}
